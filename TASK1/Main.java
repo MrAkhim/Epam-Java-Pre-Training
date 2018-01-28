88e0d18903cbd27afe082881123fccecba424e01
PishchalovaAgata <zemtsov.69@mail.ru>
2018-01-28 14:51:46 +0300

    add corrected TASK1

--- TASK1/Main.java
+++ TASK1/Main.java
@@ -1,25 +1,25 @@
 public class Main {
 
-    public static double dinosaurWeightKilogram(int weight) {
-        return weight / 1000.0;
+    private static int dinosaurWeightKilogram(int weight) {
+        return weight / 1000;
     }
 
-    public static double dinosaurWeightTon(int weight) {
-        return weight / 1000000.0;
+    private static int dinosaurWeightTon(int weight) {
+        return weight / 1000000;
     }
 
-    public static double ringSquare(double R1, double R2) {
-        return Math.PI * (R1 * R1) - Math.PI * (R2 * R2);
+    private static double ringSquare(double R1, double R2) {
+        return Math.PI * (R1 * R1 - R2 * R2);
     }
 
-    public static void swap(int a, int b) {
+    private static void swap(int a, int b) {
         a = a + b;
         b = a - b;
         a = a - b;
-        System.out.println(a + "  " + b);
+        System.out.println("a = " + a + " and b = " + b);
     }
 
-    public static boolean increasingSequanceNumber(int number) {
+    private static boolean increasingSequenceNumber(int number) {
         int copy_number = number;
         while (copy_number != 0) {
             number = copy_number;
@@ -31,8 +31,7 @@ public class Main {
         return true;
     }
 
-
-    public static int sumDigits(int number) {
+    private static int sumDigits(int number) {
         int sum = 0;
         while (number % 10 != 0) {
             int m = number % 10;
@@ -42,7 +41,7 @@ public class Main {
         return sum;
     }
 
-    public static int productDigits(int number) {
+    private static int productDigits(int number) {
         int product = 1;
         while (number % 10 != 0) {
             int m = number % 10;
@@ -52,16 +51,15 @@ public class Main {
         return product;
     }
 
-    public static double SrArifm(int number) {
-        int answer = sumDigits(number) / amountDigits(number);
-        return answer;
+    private static double ArithmeticMeanDigits(int number) {
+        return sumDigits(number) / amountDigits(number);
     }
 
-    public static double SrGeometr(int number) {
+    private static double GeometricMeanDigits(int number) {
         return Math.pow(productDigits(number), 1.0 / amountDigits(number));
     }
 
-    public static int amountDigits(int number) {
+    private static int amountDigits(int number) {
         int count = 1;
         while (number / 10 != 0) {
             count++;
@@ -70,7 +68,7 @@ public class Main {
         return count;
     }
 
-    public static int reverse(int number) {
+    private static int reverse(int number) {
         int extra = 0;
         while (number != 0) {
             extra = extra * 10 + number % 10;
@@ -80,14 +78,20 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println(dinosaurWeightKilogram(12345) + "  " + dinosaurWeightTon(12345));
+        System.out.print(12345 + " g =>   ");
+        System.out.println(dinosaurWeightKilogram(12345) + " kg  and " + dinosaurWeightTon(12345) + " tons");
+        System.out.print("the square of the ring with radios 2 and 1 is =>   ");
         System.out.println(ringSquare(2, 1));
+        System.out.print("Swap the values of a = 8 and b = 12 =>      ");
         swap(8, 12);
-        System.out.println(increasingSequanceNumber(123456));
-        System.out.println(increasingSequanceNumber(654321));
-        System.out.println(sumDigits(123456789) + "  " + productDigits(123456789));
-        System.out.println(SrArifm(123456789) + "   " + SrGeometr(123456789));
-        System.out.println(reverse(123456789));
+        System.out.println("123456 increasing sequence? => " + increasingSequenceNumber(123456));
+        System.out.println("658712 increasing sequence? => " + increasingSequenceNumber(658712));
+        System.out.println("Sum of the digits of 123456789  =>   " + sumDigits(123456789));
+        System.out.println("Product of the digits of 123456789 =>  " + productDigits(123456789));
+        System.out.println("Arithmetic mean of digits of 123456789 =>  " + ArithmeticMeanDigits(123456789));
+        System.out.println("Geometric mean of digits of 123456789 =>   " + GeometricMeanDigits(123456789));
+        System.out.println("Reverse of 123456789 =>  " + reverse(123456789));
     }
 
 }
+
